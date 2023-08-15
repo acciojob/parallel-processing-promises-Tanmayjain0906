6e762ae78fffa2811df8099a5fcee7fc53102654
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-08-15 14:08:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your JS code here. If required.
+
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +7,80 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImage() {
+	let promises = images.map((val) => {
+	let prom = new Promise((resolve, reject) => {
+		let img = new Image();
+		img.src = val.url;
+
+		img.onload = () => {
+			resolve(img);
+		}
+
+		img.onerror = () => {
+			reject(`Failed to load image's URL: ${val.url}`)
+		}
+	})
+		return prom;
+})
+
+let x = Promise.all(promises);
+	x.then((data) => {
+		data.forEach((img) => {
+			output.appendChild(img);
+		})
+	})
+
+	x.catch((data) => {
+		console.log(data);
+	})
+}
+
+btn.addEventListener("click", loadImage);
+
+
+// const output = document.getElementById("output");
+// const btn = document.getElementById("download-images-button");
+
+// const images = [
+//   { url: "https://picsum.photos/id/237/200/300" },
+//   { url: "https://picsum.photos/id/238/200/300" },
+//   { url: "https://picsum.photos/id/239/200/300" },
+// ];
+
+
+// function downloadImages() {
+
+//  let promises = images.map((val)=>{
+			
+// 		   let prom = new Promise((resolve,reject)=>{
+// 			   const img = new Image();
+// 			   img.src = val.url;
+// 			   img.alt = val.alt;
+//  			img.onload = ()=>{
+// 				resolve(img);
+// 			}
+
+// 			img.onerror = ()=>{
+// 				reject(`Failed to load image's URL: ${val.url}`)
+// 			}
+// 		});
+// 	 return prom;
+// 	});
+
+// 	 let x = Promise.all(promises)
+// 	x.then((data) =>{
+// 		data.forEach((image) =>{
+// 			output.appendChild(image);
+// 		})
+// 	})
+// 	x.catch((data)=>
+// 		{
+// 			console.log(data);
+// 		});
+
+// }
+
+
+// btn.addEventListener("click" , downloadImages);
